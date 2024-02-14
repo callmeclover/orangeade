f5253755b9903cae7e3b1c2661a060ad29ce8318
Clover Johnson <cats11134@gmail.com>
2024-02-14 13:19:48 +0000

    asasd

--- src/libfunc.rs
+++ src/libfunc.rs
@@ -1,6 +1,42 @@
+use lol_html::{element, HtmlRewriter, Settings};
+
+pub fn is_url_absolute(url: &str) -> bool {
+    url.starts_with("//") || (url.contains("://") && url.contains(".") && url.contains("/") && url.find(":") < url.find("/"))
+}
+
 // replace `source: &str` with document typing later
 pub fn replace_resources(source: &str, hostname: &str) {
     // This function has 4 parts.
+    let mut rewriter = HtmlRewriter::new(
+        Settings {
+            element_content_handlers: vec![
+                element!("link[href]", |el| {
+                    let href = el
+                        .get_attribute("href")
+                        .expect("href was required");
+                    
+                    print!(href)
+
+                    //el.set_attribute("href", &href)?;
+
+                    Ok(())
+                }),
+                element!("script[src]", |el| {
+                    let src = el
+                        .get_attribute("src")
+                        .expect("src was required");
+                    
+                    print!(src)
+
+                    //el.set_attribute("src", &src)?;
+
+                    Ok(())
+                })
+            ],
+            ..Settings::default()
+        },
+        |c: &[u8]| output.extend_from_slice(c)
+    );
 
     // 1. Get all occurences of a <link> or <script> in source.
     // 2. Regex href or src to check if it's relative or absolute.
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod libfunc;
 use libfunc::*;
 
 fn main() {
+    let hostname: &str = "https://example.com";
     let source: &str = #r"<html>
     <head>
     <link rel="stylesheet" href="/dummy.css" />
@@ -10,7 +11,8 @@ fn main() {
     <body>
     </body>
     </html>
-    "#
+    "#;
     // TODO: see that <link> and <script>? we need to replace it's href (and src) to an absolute link (via resource injection and replacement)
     // see libfunc's replace_resources for this implementation
+    replace_resources(source, hostname);
 }
